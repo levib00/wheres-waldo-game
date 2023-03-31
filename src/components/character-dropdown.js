3df703d1d827ed455f51d6071693e6a75f8c44fe
levib00 <levib8@hotmail.com>
2023-03-30 20:28:43 -0400

    Add dropdown of characters

---
+++ src/components/character-dropdown.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const CharacterDropdown = () => {
+
+  return (
+    <div>
+      <p>Hiei</p>
+      <p>Vash The Stampede</p>
+      <p>Misaka</p>
+    </div>
+  )
+}
