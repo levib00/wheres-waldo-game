de65ac15dccf6020e200dacb0fb4b4be0c01fe20
levib00 <levib8@hotmail.com>
2023-04-02 21:47:32 -0400

    Check if guess is correct

--- src/components/character-dropdown.js
+++ src/components/character-dropdown.js
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const CharacterDropdown = () => {
+export const CharacterDropdown = (props) => {
+  const {top, left, isCorrectGuess} = props
 
+  const myStyle = {
+    position: 'absolute',
+    top: top - 75,
+    left: left -75,
+    border: 'red 5px solid',
+    color: 'yellow',
+    height: '150px',
+    width: '150px'
+  }
+
+  const myStyle2 = {
+    position: 'absolute',
+    top: top + 45,
+    left: left + 79,
+    border: 'red 5px solid',
+    color: 'yellow',
+    height: '150px',
+    width: '150px',
+    backgroundColor: 'grey',
+  }
+
+  const selectChar = async(character) => {
+    if (await isCorrectGuess(character, left, top)) {
+      console.log(character, true)
+    } else {
+      console.log(character, false)
+    }
+  }
   return (
     <div>
-      <p>Hiei</p>
-      <p>Vash The Stampede</p>
-      <p>Misaka</p>
+      <div style={myStyle}>
+
+      </div>
+      <div style={myStyle2}>
+        <p onClick={() => selectChar('hiei')}>Hiei</p>
+        <p onClick={() => selectChar('vash')}>Vash The Stampede</p>
+        <p onClick={() => selectChar('misaka')}>Misaka</p>
+      </div>
     </div>
   )
 }
