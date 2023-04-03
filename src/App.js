50963fdd4feee9aad9f5152d09db4148a2d55986
levib00 <levib8@hotmail.com>
2023-04-02 21:48:47 -0400

    Add routing and imports

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Game } from "./components/game";
+import { Nav } from './components/nav-bar'
+import { Leaderboards } from "./components/leaderboards";
+ 
+const App = () => {
 
-function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path='/' element={<Game />}/>
+          <Route path='/leaderboards' element={<Leaderboards />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
