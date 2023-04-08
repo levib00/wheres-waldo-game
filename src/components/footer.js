864f8b114aacc04aa900e9292f9be00f10ae7110
levib00 <levib8@hotmail.com>
2023-04-07 20:56:32 -0400

    Add basic footer that credits artist

---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import '../styles/styles.css'
+
+export const Footer = () => {  
+  return (
+    <div className="footer">
+      <div>Art by</div>&nbsp;<a href='https://www.deviantart.com/randyadr'>randyadr</a>
+    </div>
+  )
+}
