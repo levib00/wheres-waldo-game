f86d37787b7b829d7c3f277f9f730c39794d16c2
levib00 <levib8@hotmail.com>
2023-04-07 21:07:35 -0400

    Add footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Game } from "./components/game";
 import { Nav } from './components/nav-bar'
 import { Leaderboards } from "./components/leaderboards";
+import { Footer } from "./components/footer";
+import './styles/styles.css'
  
 const App = () => {
 
@@ -14,6 +16,7 @@ const App = () => {
           <Route path='/' element={<Game />}/>
           <Route path='/leaderboards' element={<Leaderboards />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
