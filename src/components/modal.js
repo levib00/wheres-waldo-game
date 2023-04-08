bec936d4e5222db8195b2d60480d27e9516e23fa
levib00 <levib8@hotmail.com>
2023-04-07 21:05:00 -0400

    Adjust temporary styling

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,37 +1,54 @@
 import React, { useState } from "react";
+import hiei from '../assets/images/hiei.png'
+import vash from '../assets/images/vash1.png'
+import misaka from '../assets/images/misaka.png'
 
 export const Modal = (props) => {
   const {startTimer, setShow} = props
   const myStyle = {
     position : 'absolute',
-    width: '300px',
-    height: '300px',
-    top: 'calc(50% - 150px)',
-    left: 'calc(50% - 150px)',
+    width: '400px',
+    height: '700px',
+    top: 'calc(50% - 350px)',
+    left: 'calc(50% - 200px)',
     backgroundColor: 'grey',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
+    justifyContent: 'space-around',
+    padding: '5px 0px'
   }
+
+  const imgStyle = {
+    height: '200px',
+    maxWidth: '130px'
+  }
+
   const handleClick = () => {
     startTimer()
     setShow(false)
   }
-  console.log(props)
+
   return (
     <div style={myStyle}>
       <div>
-        <div>
-          <img />
-          <p>Hiei from Yu Yu Hakusho</p>
+        <div style={{display: 'flex', alignItems: 'center', margin: '5px'}}>
+          <div style={{width: '130px', display: 'flex', justifyContent: 'center'}}>
+            <img style={imgStyle} src={misaka} alt="Misaka example"/>
+          </div>
+          <p>Misaka from A Certain Scientific Railgun</p>
         </div>
-        <div>
-          <img />
+        <div style={{display: 'flex', alignItems: 'center', margin: '5px'}}>
+          <div style={{width: '130px', display: 'flex', justifyContent: 'center'}}>
+            <img style={imgStyle} src={vash} alt="Vash the Stampede example"/>
+          </div>
           <p>Vash the Stampede from Trigun</p>
         </div>
-        <div>
-          <img />
-          <p>Misaka from A Certain Scientific Railgun</p>
+        <div style={{display: 'flex', alignItems: 'center', margin: '5px'}}>
+          <div style={{width: '130px', display: 'flex', justifyContent: 'center'}}>
+            <img style={imgStyle} src={hiei} alt="Hiei example"/>
+          </div>
+          <p>Hiei from Yu Yu Hakusho</p>
         </div>
       </div>
       <button onClick={handleClick}>Start Game</button>
