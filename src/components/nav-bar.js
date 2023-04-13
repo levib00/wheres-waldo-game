a73625638aac12752c84d21dcdee5a4d64fb55f4
levib00 <levib8@hotmail.com>
2023-04-12 21:35:06 -0400

    Add tags for styling

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -2,21 +2,14 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export const Nav = () => {
-  const myStyle = {
-    display: 'flex',
-    justifyContent: 'space-between',
-    padding: '5px 50px',
-    fontSize: '1.6rem',
-    height: '40px',
-  }
   return (
-    <div style={myStyle} className="nav">
+    <div className="nav">
       <div>
-        Can You Find
+        <Link to='/'><button className="hero">Can You Find</button></Link>
       </div>
-      <div>
-        <Link to='/'><button>Play</button></Link>
-        <Link to='/leaderboards'><button>Leaderboards</button></Link>
+      <div className="nav-button-container">
+        <Link to='/'><button className="nav-button">Play</button></Link>
+        <Link to='/leaderboards'><button className="nav-button">Leaderboards</button></Link>
       </div>
     </div>
   )
