216e6fc56553a2d123d28b4baf39df1e3ea90050
levib00 <levib8@hotmail.com>
2023-04-12 21:34:29 -0400

    Add tags for styling

--- src/components/modal.js
+++ src/components/modal.js
@@ -4,25 +4,7 @@ import vash from '../assets/images/vash1.png'
 import misaka from '../assets/images/misaka.png'
 
 export const Modal = (props) => {
-  const {startTimer, setShow} = props
-  const myStyle = {
-    position : 'absolute',
-    width: '400px',
-    height: '700px',
-    top: 'calc(50% - 350px)',
-    left: 'calc(50% - 200px)',
-    backgroundColor: 'grey',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'space-around',
-    padding: '5px 0px'
-  }
-
-  const imgStyle = {
-    height: '200px',
-    maxWidth: '130px'
-  }
+  const {startTimer, setShow} = props;
 
   const handleClick = () => {
     startTimer()
@@ -30,28 +12,28 @@ export const Modal = (props) => {
   }
 
   return (
-    <div style={myStyle}>
+    <div className="modal">
       <div>
         <div style={{display: 'flex', alignItems: 'center', margin: '5px'}}>
           <div style={{width: '130px', display: 'flex', justifyContent: 'center'}}>
-            <img style={imgStyle} src={misaka} alt="Misaka example"/>
+            <img className="modal-image" src={misaka} alt="Misaka example"/>
           </div>
           <p>Misaka from A Certain Scientific Railgun</p>
         </div>
         <div style={{display: 'flex', alignItems: 'center', margin: '5px'}}>
           <div style={{width: '130px', display: 'flex', justifyContent: 'center'}}>
-            <img style={imgStyle} src={vash} alt="Vash the Stampede example"/>
+            <img className="modal-image" src={vash} alt="Vash the Stampede example"/>
           </div>
           <p>Vash the Stampede from Trigun</p>
         </div>
         <div style={{display: 'flex', alignItems: 'center', margin: '5px'}}>
           <div style={{width: '130px', display: 'flex', justifyContent: 'center'}}>
-            <img style={imgStyle} src={hiei} alt="Hiei example"/>
+            <img className="modal-image" src={hiei} alt="Hiei example"/>
           </div>
           <p>Hiei from Yu Yu Hakusho</p>
         </div>
       </div>
-      <button onClick={handleClick}>Start Game</button>
+      <button onClick={handleClick} className="start-button">Start Game</button>
     </div>
   )
 }
