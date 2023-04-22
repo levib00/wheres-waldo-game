d34f93988a9b68ac5abe41397ff6e01fe4e19d0a
levib00 <levib8@hotmail.com>
2023-04-21 21:35:39 -0400

    Add tags for styling

--- src/components/leaderboards.js
+++ src/components/leaderboards.js
@@ -15,28 +15,32 @@ export const LeaderBoards = (props) => {
   let i = 1
 
   return (
-    <div className="top-grid">
-      <h1 className="leaderboard-header">
-        Top 10
-      </h1>
-      <div className="grid-slot">
-        <div className="grid-item">Placement</div>
-        <div className="grid-item">Name</div>
-        <div className="grid-item">Time</div>
+    <div className="leaderboards">
+      <div className="top-grid">
+        <h1 className="leaderboard-header">
+          Top 10
+        </h1>
+        <div class="leaderboard-grid">
+        <div className="grid-slot">
+          <div className="grid-item">Placement</div>
+          <div className="grid-item">Name</div>
+          <div className="grid-item">Time</div>
+        </div>
+          {leaderboards.map(slot => <div key={uuidv4()} className="grid-slot">
+            <div className="grid-item">
+              {i++}
+            </div>
+            <div className="grid-item">
+              {slot.name}
+            </div>
+            <div className="grid-item">
+            {Math.floor(slot.time / 6000).toString().padStart(2, "0")}:
+            {Math.floor((slot.time % 6000) / 100).toString().padStart(2, "0")}:
+            {(slot.time % 100).toString().padStart(2, "0")}
+            </div>
+          </div>)}
+        </div>
       </div>
-      {leaderboards.map(slot => <div key={uuidv4()} className="grid-slot">
-        <div className="grid-item">
-          {i++}
-        </div>
-        <div className="grid-item">
-          {slot.name}
-        </div>
-        <div className="grid-item">
-        {Math.floor(slot.time / 6000).toString().padStart(2, "0")}:
-        {Math.floor((slot.time % 6000) / 100).toString().padStart(2, "0")}:
-        {(slot.time % 100).toString().padStart(2, "0")}
-        </div>
-      </div>)}
     </div>
   )
 }
